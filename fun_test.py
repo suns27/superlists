a88a40839108efd65ce714fa57ec2e1513b69ed5
ss <suns27@126.com>
2016-01-06 17:18:21 +0800

    10

--- fun_test.py
+++ fun_test.py
@@ -8,7 +8,8 @@ class newVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.browser.quit()
+        pass
+        #self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
@@ -25,7 +26,7 @@ class newVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:buy something' for row in rows),"new to-do item did not appear")
+        self.assertIn('1:buy something',[row.text for row in rows])
         
         self.fail('finish')
 
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -14,4 +15,16 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+        response = home_page(request)
+        #print(response.content.decode())
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string('home.html',{'new_item_text':'A new list item'})
+        print(expected_html)
+        print(response.content.decode())
+        self.assertEqual(response.content.decode(), expected_html)
 # Create your tests here.
