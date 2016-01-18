039154af52908046912dd204e10532bf72199229
suns27 <94689614@qq.com>
2016-01-18 14:36:44 +0800

    19

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', 'lists.views.home_page', name = 'home'),
-    url(r'^lists/only_list/$', 'lists.views.view_list', name = 'view_list'),
+    url(r'^lists/(\d+)/$', 'lists.views.view_list', name = 'view_list'),
+    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name = 'add_item'),
     url(r'^lists/new$', 'lists.views.new_list', name = 'new_list')
 ]
