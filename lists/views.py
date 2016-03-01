1e23e471596be8e7cc6a56e3f3c3e77e244317cb
suns27 <94689614@qq.com>
2016-03-01 10:00:06 +0800

    30

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from lists.models import Item,List
+from django.core.exceptions import ValidationError
 #from django.http import HttpResponse
 def home_page(request):
     return render(request, 'home.html')
@@ -10,7 +11,14 @@ def view_list(request,list_id):
 
 def new_list(request):
     list_=List.objects.create()
-    Item.objects.create(text=request.POST['item_text'],list=list_)
+    item = Item.objects.create(text=request.POST['item_text'],list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You cannot have an empty list item"
+        return render(request, 'home.html', {"error":error})
     return redirect('/lists/%d/' % (list_.id,))
 
 def add_item(request, list_id):
